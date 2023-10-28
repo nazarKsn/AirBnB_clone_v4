adab92481e38530f70b5aa999b588ccac721f368
Shallom Kanyori <shallomkanyori@gmail.com>
2023-10-28 12:02:50 +0000

    Add 404 error handler that returns JSON
    Add a 404 error handler that returns a JSON-formatted 404 status code
    response.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """Flask Application"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
-from os import environ
 from api.v1.views import app_views
+from os import environ
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -17,6 +19,16 @@ def close_db(error):
     """Close storage"""
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """404 Error
+
+    Responses:
+        404:
+            description: a resource was not found
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 app.config['SWAGGER'] = {
         'title': 'AirBnB clone Restful API',
