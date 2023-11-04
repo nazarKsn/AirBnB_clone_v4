6acca0ec798aee760e2b8189237acb15a5ba79ea
Shallom Kanyori <shallomkanyori@gmail.com>
2023-11-04 12:18:30 +0000

    Add html template for task 6

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -18,7 +18,8 @@ $('document').ready(function () {
     success: appendPlaces
   });
 
-  let states = {};
+  const states = {};
+  const cities = {};
   $('.locations > UL > H2 > INPUT[type="checkbox"]').change(function () {
     if ($(this).is(':checked')) {
       states[$(this).attr('data-id')] = $(this).attr('data-name');
@@ -33,8 +34,7 @@ $('document').ready(function () {
     }
   });
 
-  let cities = {};
-  $('.locations > UL > UL > LI INPUT[type="checkbox"]').change(function () {
+  $('.locations > UL > LI > UL > LI INPUT[type="checkbox"]').change(function () {
     if ($(this).is(':checked')) {
       cities[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
@@ -48,7 +48,7 @@ $('document').ready(function () {
     }
   });
 
-  let amenities = {};
+  const amenities = {};
   $('.amenities INPUT[type="checkbox"]').change(function () {
     if ($(this).is(':checked')) {
       amenities[$(this).attr('data-id')] = $(this).attr('data-name');
@@ -67,9 +67,9 @@ $('document').ready(function () {
       url: api + ':5001/api/v1/places_search/',
       type: 'POST',
       data: JSON.stringify({
-        'states': Object.keys(states),
-        'cities': Object.keys(cities),
-        'amenities': Object.keys(amenities)
+        states: Object.keys(states),
+        cities: Object.keys(cities),
+        amenities: Object.keys(amenities)
       }),
       contentType: 'application/json',
       dataType: 'json',
