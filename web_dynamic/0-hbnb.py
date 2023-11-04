0b714c4f5e1d4d1d5d736cc067c8a1db4990a8bc
Shallom Kanyori <shallomkanyori@gmail.com>
2023-11-04 09:54:02 +0000

    Fix semistandard errors

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -34,7 +34,6 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    print('Users:', users)
     cache_id = (str(uuid.uuid4()))
     return render_template('0-hbnb.html',
                            states=states,
--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,5 +1,5 @@
 $('document').ready(function () {
-  let amenities = {};
+  const amenities = {};
   $('INPUT[type="checkbox"]').change(function () {
     if ($(this).is(':checked')) {
       amenities[$(this).attr('data-id')] = $(this).attr('data-name');
